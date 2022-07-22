3312506e3a383ea5ae8f1c1671482c1124d91dcc
Bruce <ibrucejobs@outlook.com>
2022-07-22 10:04:16 +1000

    aaa

--- sort/merge_sort.cpp
+++ sort/merge_sort.cpp
@@ -1,11 +1,8 @@
-/*
-https://www.programiz.com/dsa/merge-sort
-*/
-
 #include <iostream>
 
+// [5,1,2,4,8]
 void merge(int arr[], int left, int middle, int right) {
-  // create two separate arrays
+  // copy elements to array
   int size_1 = middle - left + 1;
   int size_2 = right - middle;
 
@@ -20,38 +17,27 @@ void merge(int arr[], int left, int middle, int right) {
     arr_2[i] = arr[middle + 1 + i];
   }
 
-  // Maintain current index of sub-arrays and main array
   int i = 0;
   int j = 0;
   int k = left;
 
   while (i < size_1 && j < size_2) {
     if (arr_1[i] <= arr_2[j]) {
-      arr[k] = arr_1[i];
-      i++;
+      arr[k++] = arr_1[i++];
     } else {
-      arr[k] = arr_2[j];
-      j++;
+      arr[k++] = arr_2[j++];
     }
-    k++;
   }
 
-  // When we run out of elements in either L or M,
-  // pick up the remaining elements and put in A[p..r]
   while (i < size_1) {
-    arr[k] = arr_1[i];
-    i++;
-    k++;
+    arr[k++] = arr_1[i++];
   }
 
   while (j < size_2) {
-    arr[k] = arr_2[j];
-    j++;
-    k++;
+    arr[k++] = arr_2[j++];
   }
 }
 
-// Divide the array into two subarrays, sort them and merge them
 void divide(int arr[], int left, int right) {
   if (left < right) {
     int middle = left + (right - left) / 2;
@@ -64,6 +50,7 @@ void divide(int arr[], int left, int right) {
 int main() {
   int arr[] = {6, 5, 12, 10, 9, 1};
   int size = sizeof(arr) / sizeof(arr[0]);
+
   divide(arr, 0, size - 1);
 
   for (int i = 0; i < size; i++) {
--- sort/quick_sort.cpp
+++ sort/quick_sort.cpp
@@ -1,60 +1,65 @@
 #include <algorithm>
 #include <iostream>
 
-int partition(int arr[], int start, int end) {
-  int pivot = arr[start];
-  int count = 0;
-  for (int i = start + 1; i <= end; i++) {
-    if (arr[i] <= pivot) {
-      count++;
+/*
+https://www.youtube.com/watch?v=Hoixgm4-P4M
+
+quick sort = pivot
+1) pivot is corret position in final sorted array
+2) items to the left are smaller
+3) items to the right are larger
+
+2 6 5 3 8 7 1 0
+step:
+1.pick a pivot
+2.swap pivot with the end element
+3.find left > than pivot
+4.find right < than pivot
+5.swap left with right
+6.repeat 3~5 until left index > right index
+7.swap left with pivot(at the end index) (pivot element is in right position)
+
+how to choose pivot
+divide the array in half
+
+worst case O(n^2)
+avg case O(nlogn)
+*/
+
+int partition(int arr[], int low, int high) {
+  int left = low;
+  int pivot = arr[high];
+
+  for (int right = low; right < high; right++) {
+    if (arr[right] < pivot) {
+      std::swap(arr[left], arr[right]);
+      left++;
     }
   }
 
-  int pivot_index = start + count;
-  std::swap(arr[start], arr[pivot_index]);
-
-  int i = start, j = end;
-  while (i < pivot_index && j > pivot_index) {
-    while (arr[i] <= arr[pivot_index]) {
-      i++;
-    }
-
-    while (arr[j] > arr[pivot_index]) {
-      j--;
-    }
-
-    if (i < pivot_index && j > pivot_index) {
-      std::swap(arr[i], arr[j]);
-      i++;
-      j--;
-    }
-  }
-
-  return pivot_index;
+  std::swap(arr[left], arr[high]);
+  return left;
 }
 
-void quick_sort(int arr[], int start, int end) {
-  // base case
-  if (start >= end) {
+void quick_sort(int arr[], int low, int high) {
+  if (high <= low) {
     return;
   }
 
-  // partitioning the array
-  int q = partition(arr, start, end);
-
-  // sort the left part
-  quick_sort(arr, start, q - 1);
-
-  // sort the right part
-  quick_sort(arr, q + 1, end);
+  int p = partition(arr, low, high);
+  quick_sort(arr, low, p - 1);
+  quick_sort(arr, p + 1, high);
 }
 
 int main() {
-  int arr[] = {8, 6, 12, 7, 1};
+  int arr[] = {5, 8, 1, 10, 8, 7};
   int size = sizeof(arr) / sizeof(arr[0]);
+
   quick_sort(arr, 0, size - 1);
 
   for (int i = 0; i < size; i++) {
-    std::cout << i << std::endl;
+    std::cout << arr[i] << " ";
   }
+
+  std::cout << std::endl;
 }
